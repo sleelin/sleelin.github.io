059827b859cda71eede7592bf0fffaf0dd4d7f2e
Sam Lee-Lindsay <sleelin@gmail.com>
2025-01-30 18:40:35 +1100

    Add Google Analytics for traffic monitoring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "@sleelin/portfolito";
 import "./components/face.js";
 import "./components/toggle.js";
+import "https://www.googletagmanager.com/gtag/js?id=G-0ZJCXRJC9S";
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+gtag("config", "G-0ZJCXRJC9S");
